5cdd7e4e040a7dfcf5e6ec3ee4581d6b3fa6b89b
Ionuț G. Stan <ionut.g.stan@gmail.com>
2014-12-18 17:37:35 +0200

    Initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,7 @@
+package bjug
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hello, World!")
+  }
+}
---
+++ src/test/scala/InterpreterTest.scala
@@ -0,0 +1,10 @@
+package bjug
+package test
+
+import org.scalatest.{ FunSuite, Matchers }
+
+class InterpreterTest extends FunSuite with Matchers {
+  test("tests work") {
+    true should be(true)
+  }
+}
